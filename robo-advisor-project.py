15fad89729db10fee659c8df1f70915ab4161a45
eocello10 <50848324+eocello10@users.noreply.github.com>
2019-06-14 09:19:30 -0400

    updated date,time and working on latest_close

--- robo-advisor-project.py
+++ robo-advisor-project.py
@@ -4,6 +4,10 @@
 
 import requests
 import json
+import datetime
+
+#def to_usd (my_price):
+ #   return "{0:,.2f}".format(my_price)
 #
 #Info inputs
 #
@@ -16,21 +20,25 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
+
 # need to go to alpha key documents and get the url above for json file
 
 #using parsed_response you can find keys on specific attributes or entire list. This just finds the different values/results.
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+now = datetime.datetime.now()
+#latest_close = parsed_response["Time Series (5min)"]["2019-06-13 16:00:00"]["4. close"]
 
-
+breakpoint()
+# We are determing the different keys we need to process in python - time series daily - based on the day
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST AT: {now}")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")#string interprelation using format string
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {latest_close}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
